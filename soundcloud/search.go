464bc330dc1baab473df9433a988d026aee0de44
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-06-13 16:37:56 -0500

    [update]

--- soundcloud/search.go
+++ soundcloud/search.go
@@ -76,14 +76,14 @@ func isSong(url string) bool {
 	if err != nil {
 		panic(err)
 	}
-	// doc.Find("meta[property='twitter:title']").Each(func(i int, s *goquery.Selection) {
-	// 	// get the data from found element's content attribute
-	// 	song, exists := s.Attr("content")
-	// 	if exists {
-	// 		title = song
+	doc.Find("meta[property='SoundCloud:title']").Each(func(i int, s *goquery.Selection) {
+		// get the data from found element's content attribute
+		song, exists := s.Attr("content")
+		if exists {
+			title = song
 
-	// 	}
-	// })
+		}
+	})
 	
 	return false
 }
