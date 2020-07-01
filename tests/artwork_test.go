b45777dfe5bacc069ee266b494e055f8aab00d5c
Ghost <imthaghost@protonmail.com>
2020-06-30 18:05:43 -0700

    [update] tests folder

---
+++ tests/artwork_test.go
@@ -0,0 +1,45 @@
+package tests
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/fatih/color"
+	"github.com/imthaghost/scdl/soundcloud"
+)
+
+// Todo: Edge Cases
+func TestGetArtwork(t *testing.T) {
+	tables := []struct {
+		url      string
+		expected string
+	}{
+		{"https://soundcloud.com/uiceheidd/tell-me-you-love-me", "https://i1.sndcdn.com/artworks-DuzeporxapxyfgpP-PkGOzQ-t500x500.jpg"},
+		{"https://soundcloud.com/uiceheidd/righteous", "https://i1.sndcdn.com/artworks-aoE3z9jDcBF2yf5p-f62P6Q-t500x500.jpg"},
+	}
+	for _, table := range tables {
+		// request to user inputed SoundCloud URL
+		resp, err := http.Get(table.url)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// response
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		result, _ := soundcloud.GetArtwork(body)
+		expectedresult := table.expected
+		if result != expectedresult {
+			t.Error()
+			red := color.New(color.FgRed).SprintFunc()
+			fmt.Printf("%s GetTitle Failed: %s , expected %s got %s \n", red("[-]"), table.url, expectedresult, result)
+		} else {
+			green := color.New(color.FgGreen).SprintFunc()
+			fmt.Printf("%s Passing: %s \n", green("[+]"), table.url)
+		}
+	}
+}
---
+++ tests/title_test.go
@@ -0,0 +1,47 @@
+package tests
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/fatih/color"
+	"github.com/imthaghost/scdl/soundcloud"
+)
+
+// Todo: Edge Cases
+func TestGetTitle(t *testing.T) {
+	tables := []struct {
+		url      string
+		expected string
+	}{
+		{"https://soundcloud.com/uiceheidd/tell-me-you-love-me", "Tell Me U Luv Me (with Trippie Redd)"},
+		{"https://soundcloud.com/uiceheidd/empty", "Empty"},
+		{"https://soundcloud.com/a-boogie-wit-da-hoodie/demons-and-angels", "Demons and Angels feat. Juice WRLD"},
+		{"https://soundcloud.com/uiceheidd/im-still", "I'm Still"},
+	}
+	for _, table := range tables {
+		// request to user inputed SoundCloud URL
+		resp, err := http.Get(table.url)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// response
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		result := soundcloud.GetTitle(body)
+		expectedresult := table.expected
+		if result != expectedresult {
+			t.Error()
+			red := color.New(color.FgRed).SprintFunc()
+			fmt.Printf("%s GetTitle Failed: %s , expected %s got %s \n", red("[-]"), table.url, expectedresult, result)
+		} else {
+			green := color.New(color.FgGreen).SprintFunc()
+			fmt.Printf("%s Passing: %s \n", green("[+]"), table.url)
+		}
+	}
+}
