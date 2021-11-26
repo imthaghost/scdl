e901144e7a0a5135811d7021b69e7f1d1c10d9b5
Gary Frederick <imthaghost@protonmail.com>
2021-11-26 15:48:52 -0800

    remove print to console

--- pkg/soundcloud/title.go
+++ pkg/soundcloud/title.go
@@ -2,7 +2,6 @@ package soundcloud
 
 import (
 	"bytes"
-	"fmt"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -38,8 +37,8 @@ func GetTitle(data []byte) string {
 	title = strings.Replace(title, "?", "", -1)
 	title = strings.Replace(title, "*", "", -1)
 	title = strings.Replace(title, ":", "-", -1)
-	
-	fmt.Println(title)
+
+
 
 	return title
 }
